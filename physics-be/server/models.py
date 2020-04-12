f55957c7572795f4306051fa15e7d0d66d0c5c74
snowinmars <snowinmars@yandex.ru>
2020-04-12 16:32:57 +0400

    Add user endpoints

--- physics-be/server/models.py
+++ physics-be/server/models.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 import json
+import jwt
+import datetime
 import enum
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -9,7 +13,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_date = db.Column(db.BigInteger)
     updated_date = db.Column(db.BigInteger)
-    number = db.Column(db.String(16))
+    number = db.Column(db.String(16), unique=True)
     latex = db.Column(db.String(4096))
     image_hrefs_json = db.Column(db.String(4096))
     hints = db.relationship('Hint', backref='task', lazy='dynamic')
@@ -32,6 +36,11 @@ class HintStatus(enum.Enum):
     declined = "declined"
 
 
+class UserRole(enum.Enum):
+    user = "user"
+    admin = "admin"
+
+
 class Hint(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_date = db.Column(db.BigInteger)
@@ -51,3 +60,48 @@ class Hint(db.Model):
             'status': self.status,
             'image_hrefs_json': self.image_hrefs_json
         })
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_date = db.Column(db.BigInteger)
+    updated_date = db.Column(db.BigInteger)
+    login = db.Column(db.String(256), unique=True)
+    password_hash = db.Column(db.String(128))  # hash stores as string
+    role = db.Column(db.Enum(UserRole))
+    auth_token = db.Column(db.String(512))  # jwt has no max length, but 512 is fine for now
+
+    def __repr__(self):
+        return json.dumps({
+            'id': self.id,
+            'created_date': self.created_date,
+            'updated_date': self.updated_date,
+            'login': self.login,
+            'role': self.role.value
+        })
+
+    def set_password_hash(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    def encode_auth_token(user_id, secret):
+        now = datetime.datetime.utcnow()
+
+        payload = {
+            'exp': now + datetime.timedelta(days=0, seconds=500),
+            'iat': now,
+            'sub': user_id
+        }
+
+        return jwt.encode(
+            payload,
+            secret,
+            algorithm='HS256'
+        )
+
+    @staticmethod
+    def decode_auth_token(auth_token, secret):
+        return jwt.decode(auth_token, secret)
--- physics-be/server/server.py
+++ physics-be/server/server.py
@@ -4,25 +4,44 @@ import time
 from flask import Flask, jsonify, abort, request, Response
 from flask_migrate import Migrate
 from sqlalchemy import and_, or_
-from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, SECRET_JWT_KEY, SECURITY_PASSWORD_SALT
 from flask_jsonschema_validator import JSONSchemaValidator
+from flask_security.utils import hash_password
+from flask_security import Security, SQLAlchemyUserDatastore
 
-from models import db, Task, Hint, HintStatus
-import tasks_helpers, hints_helpers
+from models import db, Task, Hint, HintStatus, User
+import tasks_helpers, hints_helpers, users_helpers
 
 app = Flask(__name__)
 JSONSchemaValidator(app=app, root="schemas")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_MIGRATE_REPO"] = SQLALCHEMY_MIGRATE_REPO
+app.config['SECRET_JWT_KEY'] = SECRET_JWT_KEY
+app.config['SECURITY_PASSWORD_SALT'] = SECURITY_PASSWORD_SALT
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
-
 # todo: extract to controller files
 
 # helpers
+
+
+def is_in_active_role(request, roles):
+    bearer_token = request.headers.get('Authorization')
+    bearer_value = bearer_token.split()[1]
+
+    user = db.session.query(User).filter_by(auth_token=f'{bearer_value}').first()
+
+    if user is None:
+        return False
+
+    if users_helpers.check_is_token_expired(user, app.config['SECRET_JWT_KEY']):
+        return False
+
+    return user.role.value in roles
+
 
 @app.route('/api/health', methods=['GET'])
 def health():
@@ -39,6 +58,9 @@ def on_validation_error(e):
 
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
+    if not is_in_active_role(request, ['user', 'admin']):
+        abort(403)
+
     filter_number = request.args.get('filter_by_number')
 
     if filter_number:
@@ -51,6 +73,9 @@ def get_tasks():
 
 @app.route('/api/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
+    if not is_in_active_role(request, ['user', 'admin']):
+        abort(403)
+
     task = db.session.query(Task).get(task_id)
 
     if not task:
@@ -62,6 +87,9 @@ def get_task(task_id):
 @app.route('/api/tasks', methods=['POST'])
 @app.validate('task', 'upsert')
 def upsert_task():
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
     body = request.json
     task = tasks_helpers.from_model(body)
     should_insert = task.id is None
@@ -99,6 +127,9 @@ def upsert_task():
 
 @app.route('/api/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
     db.session.query(Task).filter(Task.id == task_id).delete(synchronize_session=False)
     db.session.commit()
 
@@ -110,6 +141,9 @@ def delete_task(task_id):
 
 @app.route('/api/tasks/<int:task_id>/hints', methods=['GET'])
 def get_hints(task_id):
+    if not is_in_active_role(request, ['user', 'admin']):
+        abort(403)
+
     tasks = db.session.query(Hint).filter_by(task_id=task_id).all()
 
     return jsonify(hints_helpers.to_models_list(tasks))
@@ -117,6 +151,9 @@ def get_hints(task_id):
 
 @app.route('/api/tasks/<int:task_id>/hints/<int:hint_id>', methods=['DELETE'])
 def delete_hint(task_id, hint_id):
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
     db.session.query(Hint).filter(Hint.id == hint_id).delete(synchronize_session=False)
     db.session.commit()
 
@@ -133,12 +170,18 @@ def upsert_hint(task_id):
     now = int(time.time() * 1000)  # ms
 
     if should_insert:
+        if not is_in_active_role(request, ['user', 'admin']):
+            abort(403)
+
         hint.created_date = now
         hint.updated_date = now
         hint.status = HintStatus.pending
 
         db.session.add(hint)
     else:
+        if not is_in_active_role(request, ['admin']):
+            abort(403)
+
         db_hint = db.session.query(Hint).filter_by(id=hint.id).first()
 
         if db_hint is None:
@@ -165,6 +208,9 @@ def upsert_hint(task_id):
 
 @app.route('/api/tasks/<int:task_id>/hints/<int:hint_id>/approve', methods=['POST'])
 def enable_hint(task_id, hint_id):
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
     now = int(time.time() * 1000)  # ms
     db_hint = db.session.query(Hint).filter(Hint.id == hint_id).filter(Hint.status != HintStatus.approved).first()
 
@@ -181,6 +227,9 @@ def enable_hint(task_id, hint_id):
 
 @app.route('/api/tasks/<int:task_id>/hints/<int:hint_id>/decline', methods=['POST'])
 def disable_hint(task_id, hint_id):
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
     now = int(time.time() * 1000)  # ms
     db_hint = db.session.query(Hint).filter(Hint.id == hint_id).filter(Hint.status != HintStatus.declined).first()
 
@@ -193,6 +242,142 @@ def disable_hint(task_id, hint_id):
     db.session.commit()
 
     return jsonify({'id': hint_id})
+
+
+# users
+
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
+    filter_login = request.args.get('filter_by_login')
+
+    if filter_login:
+        users = db.session.query(User).filter_by(login=filter_login).all()
+        return jsonify(users_helpers.to_models_list(users))
+
+    users = db.session.query(User).all()
+    return jsonify(users_helpers.to_models_list(users, app.config['SECRET_JWT_KEY']))
+
+
+@app.route('/api/users/<int:user_id>', methods=['POST'])
+@app.validate('user', 'update')
+def update_user(user_id):
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
+    body = request.json
+    user, _ = users_helpers.from_register_model(body)
+    db_user = db.session.query(User).filter_by(id=user_id).first()
+
+    if db_user is None:
+        abort(404)
+
+    if user.login:
+        db_user.login = user.login
+
+    if user.role:
+        db_user.role = user.role
+
+    db.session.commit()
+
+    return jsonify({'id': db_user.id})
+
+
+@app.route('/api/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
+    db_user = db.session.query(User).filter_by(id=user_id).first()
+
+    if db_user is None:
+        abort(404)
+
+    return jsonify(users_helpers.to_model(db_user, app.config['SECRET_JWT_KEY']))
+
+
+@app.route('/api/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    if not is_in_active_role(request, ['admin']):
+        abort(403)
+
+    db.session.query(User).filter(User.id == user_id).delete(synchronize_session=False)
+    db.session.commit()
+
+    return jsonify({'id': user_id})
+
+
+@app.route('/api/users/register', methods=['POST'])
+@app.validate('user', 'register')
+def register_user():
+    body = request.json
+    user, password = users_helpers.from_register_model(body)
+
+    now = int(time.time() * 1000)  # ms
+
+    if users_helpers.does_login_exists(db.session, user.login):
+        abort(409)
+
+    user.auth_token = user.encode_auth_token(user.id, app.config['SECRET_JWT_KEY']).decode()
+    user.created_date = now
+    user.updated_date = now
+    user.role = 'user'
+    user.set_password_hash(password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        'id': user.id,
+        'token': user.auth_token
+    })
+
+
+@app.route('/api/users/login', methods=['POST'])
+@app.validate('user', 'register')
+def login():
+    body = request.json
+    user, password = users_helpers.from_register_model(body)
+    now = int(time.time() * 1000)  # ms
+
+    user = db.session.query(User).filter_by(login=user.login).first()
+
+    if user is None:
+        abort(404)
+
+    if not user.check_password(password):
+        abort(403)
+
+    auth_token = user.encode_auth_token(user.id, app.config['SECRET_JWT_KEY']).decode()
+    user.auth_token = auth_token  # todo add last login time and ip
+    user.updated_date = now
+    db.session.commit()
+
+    return jsonify({
+        'id': user.id,
+        'token': auth_token
+    })
+
+
+@app.route('/api/users/logout', methods=['POST'])
+def logout():
+    bearer_token = request.headers.get('Authorization')
+    bearer_value = bearer_token.split()[1]
+    now = int(time.time() * 1000)  # ms
+
+    db_user = db.session.query(User).filter_by(auth_token=f'{bearer_value}').first()
+
+    if db_user is None:
+        return abort(404)
+
+    db_user.auth_token = None
+    db_user.updated_date = now
+    db.session.commit()
+
+    return jsonify({'id': db_user.id})
 
 
 if __name__ == '__main__':
