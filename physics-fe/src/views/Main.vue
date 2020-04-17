9249fbc18f253f019715043bb59280d0ce520628
Andrew Bokhonov <bokhonov7@gmail.com>
2020-04-17 21:26:42 +0400

    changed a lot

---
+++ physics-fe/src/views/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "HelloWorld",
+  
+  computed: mapGetters(["getShowing"]),
+
+  data() {
+    return {
+      url: "http://127.0.0.1:5000/api/",
+      num: "",
+      hints: 0,
+      solution: false,
+    };
+  },
+  methods: {
+    ...mapActions(["getTaskByNum"]),
+    getTask() {
+      this.getTaskByNum(this.num);
+      console.log(this.getShowing);
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
