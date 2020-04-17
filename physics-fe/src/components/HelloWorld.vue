d9458f98ab202bd61fe1bf40dec77c4ad9c043f8
Andrew Bokhonov <bokhonov7@gmail.com>
2020-04-17 12:55:49 +0400

    Revert "before pull"
    This reverts commit 1b7c26376e957e340018405a0d5cba9e76c4cb5b.

--- physics-fe/src/components/HelloWorld.vue
+++ physics-fe/src/components/HelloWorld.vue
@@ -1,36 +1,21 @@
 <template>
   <div class="hello">
-    <h1>Вы авторизованы как {{ user==="user" ? "Пользователь" : "Админ"}}</h1>
+    <h1>Вы авторизованы как {{ user === "user" ? "Пользователь" : "Админ"}}</h1>
     <form method="get">
       <div class="buttondiv">
         <input type="text" placeholder="Искать задачу по номеру..." />
         <br />
-        <input type="submit" value="Искать" @click="getTask" />
+        <input type="submit" />
       </div>
     </form>
-    <div>
-      <div>{{getShowing.text}}</div>
-      <div v-for = "item in getShowing.hints" :key = "item.id" ><p v-if= "item.id - 1 !== hintsOpen">{{item.text}}</p></div>
-      <div v-if = "solve"> {{getShowing.solution}}</div> 
-    </div>
   </div>
 </template>
 
 <script>
-/* eslint-disable */
-import { mapActions , mapGetters} from "vuex";
 export default {
   name: "HelloWorld",
   props: {
     user: String
-  },
-  computed: mapGetters(["getShowing"]),
-  data() {
-    return { hintsOpen: 0, solve: false };
-  },
-  methods: {
-    ...mapActions(["getTask"]),
-  
   }
 };
 </script>
@@ -41,11 +26,7 @@ export default {
   margin-right: auto;
   text-align: center;
   input {
-    width: 100%;
-    height: 20px;
-    &[type="submit"] {
-      width: 10%;
-    }
+    width: 100%
   }
 }
 h3 {
