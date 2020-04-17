1b7c26376e957e340018405a0d5cba9e76c4cb5b
Andrew Bokhonov <bokhonov7@gmail.com>
2020-04-17 12:36:13 +0400

    before pull

--- physics-fe/src/components/HelloWorld.vue
+++ physics-fe/src/components/HelloWorld.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="hello">
-    <h1>Вы авторизованы как {{ user === "user" ? "Пользователь" : "Админ"}}</h1>
+    <h1>Вы авторизованы как {{ user==="user" ? "Пользователь" : "Админ"}}</h1>
     <form method="get">
       <div class="buttondiv">
         <input type="text" placeholder="Искать задачу по номеру..." />
         <br />
-        <input type="submit" />
+        <input type="submit" value="Искать" @click="getTask" />
       </div>
     </form>
+    <div>
+      <div>{{getShowing.text}}</div>
+      <div v-for = "item in getShowing.hints" :key = "item.id" ><p v-if= "item.id - 1 !== hintsOpen">{{item.text}}</p></div>
+      <div v-if = "solve"> {{getShowing.solution}}</div> 
+    </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+import { mapActions , mapGetters} from "vuex";
 export default {
   name: "HelloWorld",
   props: {
     user: String
+  },
+  computed: mapGetters(["getShowing"]),
+  data() {
+    return { hintsOpen: 0, solve: false };
+  },
+  methods: {
+    ...mapActions(["getTask"]),
+  
   }
 };
 </script>
@@ -26,7 +41,11 @@ export default {
   margin-right: auto;
   text-align: center;
   input {
-    width: 100%
+    width: 100%;
+    height: 20px;
+    &[type="submit"] {
+      width: 10%;
+    }
   }
 }
 h3 {
--- physics-fe/src/store/index.js
+++ physics-fe/src/store/index.js
@@ -4,8 +4,85 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    showingTask: {},
+  },
+  mutations: {
+    changeTasks(state, tasks) {
+      state.showingTask = tasks
+    },
+
+  },
+  actions: {
+    getTask(ctx, num) {
+      let tasks
+      fetch('http://127.0.0.1:5000/api/tasks?page=0&count=3800&order=number').then((res) => { tasks = res.json() })
+      for (let n of tasks) {
+        if (n.number === num) {
+          ctx.commit("changeTasks", n)
+        }
+      }
+    },
+    suggestTask(ctx, data) {
+      fetch('http://127.0.0.1:5000/api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'applicatioon/json; charset = utf-8'
+        },
+        body: JSON.stringify(data)
+      })
+    },
+    suggestHint(ctx, data) {
+      fetch('http://127.0.0.1:5000/api/tasks/' + data.id + '/hints', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'applicatioon/json; charset = utf-8'
+        },
+        body: JSON.stringify(data)
+      })
+    },
+    suggestHintChange(ctx, data) {
+      fetch(`http://127.0.0.1:5000/api/tasks/${data.idone}/hints/${data.idtwo}/suggestions`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'applicatioon/json; charset = utf-8'
+        },
+        body: JSON.stringify(data)
+      })
+    },
+    approveHintChange(ctx, data) {
+      fetch(`http://127.0.0.1:5000/api/tasks/${data.idone}/hints/${data.idtwo}/suggestions/${data.idthree}/approve`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'applicatioon/json; charset = utf-8'
+        },
+        body: JSON.stringify(data)
+      })
+    },
+    declinehintChange(ctx, data) {
+      fetch(`http://127.0.0.1:5000/api/tasks/${data.idone}/hints/${data.idtwo}/suggestions/${data.idthree}/decline`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'applicatioon/json; charset = utf-8'
+        },
+        body: JSON.stringify(data)
+      })
+    },
+    delTask(ctx, data) {
+      fetch('http://127.0.0.1:5000/api/tasks/1', {
+        method: 'DEL',
+        headers: {
+          'Content-type': 'applicatioon/json; charset = utf-8'
+        },
+        body: JSON.stringify(data)
+      })
+    },
+    
+  },
+  getters: {
+    getShowing(state) {
+      return state.showingTask
+    }
+  },
+  setters: {},
 });
