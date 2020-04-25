0a4dc5eeec136adf6a8f24bf674eedfa44ec6fc7
snowinmars <snowinmars@yandex.ru>
2020-04-25 22:34:44 +0300

    Fix mobile layout for task upsert page

--- physics-fe/src/components/TaskUpsert.vue
+++ physics-fe/src/components/TaskUpsert.vue
@@ -1,42 +1,44 @@
 <template>
   <div class="ph-task-upsert">
-    <form class="ph-task-upsert-form" @submit.prevent="onSubmit">
-      <md-field>
-        <label>Номер задачи через точку...</label>
-        <md-input type="text" v-model="number" />
+    <form class="ph-form" @submit.prevent="onSubmit">
+      <div class="ph-params-wrapper">
+        <md-field>
+          <label>Номер задачи через точку...</label>
+          <md-input type="text" v-model="number" />
 
-        <span>
-          <md-icon>help_outline</md-icon>
+          <span>
+            <md-icon>help_outline</md-icon>
 
-          <md-tooltip id="number_help">Например, "2.27"</md-tooltip>
-        </span>
-      </md-field>
+            <md-tooltip id="number_help">Например, "2.27"</md-tooltip>
+          </span>
+        </md-field>
 
-      <md-field>
-        <label for="movie">Isbn 3800...</label>
-        <md-select v-model="isbn" name="movie" id="movie">
-          <md-option value="fight-club">Fight Club</md-option>
-          <md-option value="godfather">Godfather</md-option>
-          <md-option value="godfather-ii">Godfather II</md-option>
-          <md-option value="godfather-iii">Godfather III</md-option>
-          <md-option value="godfellas">Godfellas</md-option>
-          <md-option value="pulp-fiction">Pulp Fiction</md-option>
-          <md-option value="scarface">Scarface</md-option>
-        </md-select>
+        <md-field>
+          <label for="movie">Isbn 3800...</label>
+          <md-select v-model="isbn" name="movie" id="movie">
+            <md-option value="fight-club">Fight Club</md-option>
+            <md-option value="godfather">Godfather</md-option>
+            <md-option value="godfather-ii">Godfather II</md-option>
+            <md-option value="godfather-iii">Godfather III</md-option>
+            <md-option value="godfellas">Godfellas</md-option>
+            <md-option value="pulp-fiction">Pulp Fiction</md-option>
+            <md-option value="scarface">Scarface</md-option>
+          </md-select>
 
-        <span>
-          <md-icon>help_outline</md-icon>
+          <span>
+            <md-icon>help_outline</md-icon>
 
-          <md-tooltip id="isbn_help">ISBN - это уникальный номер книги из 11 или 13 символов. <br /> Найти его можно на первых или на последних страницах книги.</md-tooltip>
-        </span>
-      </md-field>
+            <md-tooltip id="isbn_help">ISBN - это уникальный номер книги из 11 или 13 символов. <br /> Найти его можно на первых или на последних страницах книги.</md-tooltip>
+          </span>
+        </md-field>
+      </div>
 
       <md-field class="ph-task-upsert-latex">
         <label>LaTeX...</label>
         <md-textarea cols="30" rows="15" v-model="latex"> </md-textarea>
 
         <div>
-          <vue-mathjax :formula="latex"></vue-mathjax>
+          <vue-mathjax class="ph-mathjax" :formula="latex"></vue-mathjax>
         </div>
       </md-field>
 
@@ -100,12 +102,29 @@ export default {
 .ph-task-upsert {
   padding: 2em;
 
-  .ph-task-upsert-form {
+  .ph-form {
     display: flex;
     flex-wrap: wrap;
 
     .md-field {
       width: 100%;
+    }
+
+    .ph-params-wrapper {
+      display: flex;
+      flex-wrap: wrap;
+      width: 100%;
+
+      .md-field {
+        margin-left: 1em;
+        margin-right: 1em;
+
+        width: 100%;
+
+        @media (min-width: 756px) {
+          width: 46%;
+        }
+      }
     }
   }
 
@@ -117,14 +136,47 @@ export default {
 
   .ph-task-upsert-latex {
     display: flex;
+    flex-direction: column;
 
     textarea {
-      border-right: 2px dotted $secondary-bg-color;
+      border-right: none;
+      border-bottom: 2px dotted $secondary-bg-color;
     }
 
     textarea,
     div {
-      width: 50%;
+      width: 100%;
+    }
+
+    div {
+      display: block;
+      padding: 1em;
+      height: 10em;
+      width: 100%;
+
+      overflow: overlay;
+    }
+  }
+
+  @media (min-width: 756px) {
+    .ph-task-upsert-latex {
+      display: flex;
+      flex-direction: row;
+
+      textarea {
+        border-right: 2px dotted $secondary-bg-color;
+        border-bottom: none;
+      }
+
+      textarea,
+      div {
+        width: 50%;
+      }
+
+      div {
+        display: inline;
+        padding: 1em;
+      }
     }
   }
 }
