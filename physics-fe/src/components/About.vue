cfe7c76c9e6611714b216426bb63ab8518632e90
Andrew Bokhonov <bokhonov7@gmail.com>
2020-04-25 17:13:57 +0400

    color palette fixed

--- physics-fe/src/components/About.vue
+++ physics-fe/src/components/About.vue
@@ -60,6 +60,29 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+
+@import "../config/variables.scss";
+.md-field,
+  .md-field.md-theme-default.md-focused,
+  .md-field.md-theme-default.md-focused,
+  .md-field.md-theme-default.md-has-value {
+    border-bottom: 1px solid $primary-fg-color;
+
+    input.md-input {
+      color: inherit;
+      -webkit-text-fill-color: $primary-fg-color;
+    }
+    textarea.md-textarea {
+      color: inherit;
+      -webkit-text-fill-color: $primary-fg-color;
+      border: 1px solid $primary-fg-color;
+    }
+    label {
+      padding-left: 1em;
+      color: $secondary-bg-color;
+    }
+    
+  }
 p {
   color: white;
 }
--- physics-fe/src/components/Reg.vue
+++ physics-fe/src/components/Reg.vue
@@ -37,6 +37,27 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+@import "../config/variables.scss";
+.md-field,
+  .md-field.md-theme-default.md-focused,
+  .md-field.md-theme-default.md-focused,
+  .md-field.md-theme-default.md-has-value {
+    border-bottom: 1px solid $primary-fg-color;
+    input.md-input {
+      color: inherit;
+      -webkit-text-fill-color: $primary-fg-color;
+    }
+    textarea.md-textarea {
+      color: inherit;
+      -webkit-text-fill-color: $primary-fg-color;
+      border: 1px solid $primary-fg-color;
+    }
+    label {
+      padding-left: 1em;
+      color: $secondary-bg-color;
+    }
+    
+  }
 p {
   color: white;
 }
@@ -55,7 +76,7 @@ input {
 .ph-main {
   margin: auto;
   margin-top: 150px;
-  width: 300px;
+  width: 400px;
   border: 3px solid lightgray;
   text-align: center;
 }
