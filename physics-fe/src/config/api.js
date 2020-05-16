e70a7df14d0fbca370b2f72deda3d911a2dc6e50
snowinmars <snowinmars@yandex.ru>
2020-05-17 01:42:15 +0300

    Revert config changes.
    I should do something with them

--- physics-fe/src/config/api.js
+++ physics-fe/src/config/api.js
@@ -1,6 +1,6 @@
 export default {
-  // apiPrefix: "http://ec2-3-120-108-152.eu-central-1.compute.amazonaws.com:5000/api",
-  apiPrefix: "http://127.0.0.1:5000/api",
+  apiPrefix: "http://ec2-3-120-108-152.eu-central-1.compute.amazonaws.com:5000/api",
+  // apiPrefix: "http://127.0.0.1:5000/api",
   defaultUser: {
     is_token_expired: true,
     role: null,
