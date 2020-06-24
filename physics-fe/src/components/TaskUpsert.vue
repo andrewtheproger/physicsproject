1782a6e470a9bd052106fdcbcef70e47007c7759
snowinmars <snowinmars@yandex.ru>
2020-06-24 17:07:22 +0300

    Remove unnecessary task validation

--- physics-fe/src/components/TaskUpsert.vue
+++ physics-fe/src/components/TaskUpsert.vue
@@ -39,9 +39,6 @@
         <span class="md-error" v-if="!$v.form.latex.required"
           >Задача должна иметь условие</span
         >
-        <span class="md-error" v-if="!$v.form.latex.mustSeemOk"
-          >В задаче из 3800 не может идти речь про LaTeX</span
-        >
       </md-field>
 
       <multiple-file-uploader
@@ -152,14 +149,7 @@ export default {
   validations: {
     form: {
       latex: {
-        required,
-        mustSeemOk(v) {
-          if (v) {
-            return !v.includes("LaTeX");
-          }
-
-          return true;
-        }
+        required
       },
       number: {
         required,
