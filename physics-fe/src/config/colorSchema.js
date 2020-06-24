698217b5c90d98cafb92fbbde214cb48b243aeb4
snowinmars <snowinmars@yandex.ru>
2020-06-24 10:10:49 +0300

    Add two color schemas

---
+++ physics-fe/src/config/colorSchema.js
@@ -0,0 +1,30 @@
+export default {
+  dark: {
+    color_background_primary: "#333333",
+    color_background_secondary: "#666666",
+    color_background_action: "#336699",
+    color_foreground_primary: "#cccccc",
+    color_foreground_secondary: "#ccccff",
+    color_foreground_action: "#ffffff",
+    color_background_success: "#006600",
+    color_background_warning: "#666600",
+    color_background_error: "#660000",
+    color_foreground_success: "#33cc66",
+    color_foreground_warning: "#cccc33",
+    color_foreground_error: "#dd3333"
+  },
+  light: {
+    color_background_primary: "#ffffff",
+    color_background_secondary: "#cccccc",
+    color_background_action: "#336699",
+    color_foreground_primary: "#330000",
+    color_foreground_secondary: "#003333",
+    color_foreground_action: "#ffffff",
+    color_background_success: "#99cc99",
+    color_background_warning: "#cc9966",
+    color_background_error: "#cc6666",
+    color_foreground_success: "#333333",
+    color_foreground_warning: "#333333",
+    color_foreground_error: "#333333"
+  }
+}
